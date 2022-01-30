e8c27b5116daba6c26beae9f1e0209659d8e6eb8
Swebert Correa <correaswebert@gmail.com>
2022-01-30 22:38:26 +0530

    WIP: using store from ts file

--- src/components/PlayRow.svelte
+++ src/components/PlayRow.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
   import PlayIcon from "$icons/PlayIcon.svelte";
-  import { dimens, grid } from "../utils/store";
-  import { playRow } from "../utils/music.svelte";
+  import { dimens } from "../utils/store";
+  import { playRow } from "../utils/music2";
 
   function handlePlay(idx: number) {
-    playRow($grid[idx])
+    playRow(idx);
   }
 </script>
 
 <div class="flex flex-col">
   {#each Array($dimens.numRows) as _, idx}
-    <button
-      class="
-        w-24 h-24
-        m-1
-        flex justify-center items-center
-        border border-zinc-900 rounded-lg
-        transition
-        active:bg-white"
-      on:click|capture={() => handlePlay(idx)}
-    >
+    <button on:click|capture={() => handlePlay(idx)}>
       <PlayIcon />
     </button>
   {/each}
 </div>
+
+<style>
+  button {
+    @apply w-24 h-24
+    m-1
+    flex justify-center items-center
+    border border-zinc-900 rounded-lg
+    transition
+    active:bg-white;
+  }
+</style>
