801aafbb67405b879bbd0cf0adf84ecde41d3809
Swebert Correa <correaswebert@gmail.com>
2022-01-31 00:16:30 +0530

    removed unused clearGrid export

--- src/components/Grid2.svelte
+++ src/components/Grid2.svelte
@@ -18,18 +18,6 @@
     $dimens.numCols = synthData.grid[0].length;
   }
 
-  export function clearGrid() {
-    if (!$grid.length) return;
-
-    for (let row = 0; row < $dimens.numRows; row++) {
-      for (let col = 0; col < $dimens.numCols; col++) {
-        $grid[row][col] = false;
-      }
-    }
-
-    $grid = $grid;
-  }
-
   function updateGrid(newNumRows: number, newNumCols: number): void {
     const oldGrid = [...$grid];
     $grid = [];
